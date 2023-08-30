e312ae2b9e28af713b45854af8c18e3ccbf5f7e8
Swtpea1516 <134784015+Swtpea1516@users.noreply.github.com>
2023-08-30 16:12:03 +0000

    reallydone

--- src/app.js
+++ src/app.js
@@ -2,17 +2,21 @@ import "./style.css";
 
 window.onload = function() {
   const pronoun = [`the`, `our`];
-  const adj = [`great`, `big`];
-  const noun = [`jogger`, `racoon`];
-  let domain = document.getElementById("domain");
+  const adj = [`great`, `big`, "cool", "reliable", "progressive", "super"];
+  const noun = [`jogger`, `racoon`, "code", "foundation", "cat", "roots"];
+  const domainGen = [`.com`, `.org`, `.net`];
+  var domainList = [];
+
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        domain.innerHTML +=
-          pronoun[i] + adj[j] + noun[k] + ".com" + "<br></br>";
-
-        console.log(domain);
+        domainList.push(pronoun[i] + adj[j] + noun[k] + ".com");
       }
     }
   }
+  let domain = document.querySelector("#domain");
+  for (let i = 0; i < domainList.length; i++) {
+    domain.innerHTML += "<li>" + domainList[i] + "<li>";
+  }
 };
+c;
